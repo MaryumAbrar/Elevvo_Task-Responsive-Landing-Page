159675e5e773cb09e91902d067e794c02e978310
MaryumAbrar <maryumbrr@gmail.com>
2025-10-19 10:13:13 +0500

    Changes Done!

--- app.js
+++ app.js
@@ -4,3 +4,10 @@ themeToggle.addEventListener("click", () => {
   document.body.classList.toggle("dark");
   themeToggle.textContent = document.body.classList.contains("dark") ? "☀️" : "🌙";
 });
+
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
